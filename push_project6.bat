e1abc2d6bf2c02e9b99b14fbc7c71f2c9557b17e
Saz Amin <neobaul@gmail.com>
2026-06-27 14:44:23 -0400

    Initial commit

---
+++ push_project6.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d "F:\GEN_AI\final submission\project 6 Search engine with langchain tools & agents"
+
+git add .
+git commit -m "Initial commit"
+git push -u origin main --force
+
+echo Done!
+pause
